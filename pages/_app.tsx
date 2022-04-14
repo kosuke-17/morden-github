45ae5e068a6e3dd8b1243a2ce0c73d3b3b89a3c0
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2022-04-14 10:52:37 +0900

    GrapfQLとApolloClientの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Header } from "../components";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <Header />
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Header />
+        <Component {...pageProps} />
+      </ApolloProvider>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,40 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 
-const Home: NextPage = () => {
-  return <div className={styles.container}></div>;
+// クエリ文用変数
+const GET_SAMPLE = gql`
+  {
+    viewer {
+      login
+      name
+      followers {
+        totalCount
+      }
+      following {
+        totalCount
+      }
+    }
+  }
+`;
+// SSGでのデータ取得方法
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await client.query({
+    query: GET_SAMPLE,
+  });
+  // データ取得確認用console.log
+  console.log(data);
+
+  return {
+    props: { data },
+  };
+};
+
+const Home: NextPage = ({
+  data,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return <div className={styles.container}>ログイン：{data.viewer.login}</div>;
 };
 
 export default Home;
