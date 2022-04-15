a60f58ec5fb07ac334d816f5d9abddfc2042e60a
kosuke-17 <tamutamu17@icloud.com>
2022-04-15 11:31:55 +0900

    ログインをformik適用

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import styled from "styled-components";
 import githubLogo from "../public/github.png";
 import EmailIcon from "@mui/icons-material/Email";
 import LockIcon from "@mui/icons-material/Lock";
 import { LoginType } from "../utils/Types";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Link from "next/link";
 
 //  styled-components
 // ----------------------------------------------
@@ -24,7 +27,7 @@ const GlassCard = styled.div`
     linear-gradient(0deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0));
 `;
 const CardContent = styled.div``;
-const FormContent = styled.div`
+const FormContent = styled.form`
   display: grid;
   place-items: center;
 `;
@@ -44,12 +47,54 @@ const PasswordBlock = styled.div`
 `;
 const PasswordInput = styled.input``;
 const LoginButton = styled.button``;
+const ErrMsg = styled.span`
+  padding: 4px;
+
+  font-size: 16px;
+  color: #fd7979;
+`;
+const ErrCard = styled.div`
+  margin-left: 10px;
+  max-width: 300px;
+  text-align: center;
+  border-radius: 8%;
+  box-shadow: 2px 2px 4px gray;
+  border: solid 1px rgba(0, 0, 0, 0.1);
+  background: linear-gradient(0deg, #ffecec, #ffecec),
+    linear-gradient(0deg, #ffffff, #ffffff);
+`;
 // ----------------------------------------------
 
-const Login: FC<LoginType> = ({ isLogin, setIsLogin }) => {
-  const loginClick = () => {
+const Login: FC<LoginType> = ({ isLogin, setIsLogin, getAccessToken }) => {
+  // ログインする処理
+  const loginClick = (
+    values: { email: string; password: string },
+    { resetForm }: any
+  ) => {
+    // 真偽値を変える
     setIsLogin(!isLogin);
+    // valueの値をアラート
+    alert(JSON.stringify(values, null, 2));
+    // valueの値をリセット
+    resetForm();
   };
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .max(10, "10字以下で入力してください")
+        .required("メールアドレスが空欄です"),
+      password: Yup.string()
+        .max(10, "10字以下で入力してください")
+        .required("パスワードが空欄です"),
+    }),
+    onSubmit: loginClick,
+  });
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  let loginURL = `https://github.com/login/oauth/authorize?client_id=ad5ca84d2cedcaf66be1`;
   return (
     <LoginLayout>
       <GlassCard>
@@ -62,17 +107,48 @@ const Login: FC<LoginType> = ({ isLogin, setIsLogin }) => {
               height={200}
             />
           </CardImg>
-          <FormContent>
+          <FormContent onSubmit={formik.handleSubmit}>
             <EmailBlock>
               <EmailIcon fontSize="large" /> &nbsp;
-              <EmailInput />
+              <EmailInput
+                id="email"
+                name="email"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
+              {/* エラー表示 */}
+              {formik.touched.email && formik.errors.email ? (
+                <ErrCard>
+                  <ErrMsg>{formik.errors.email}</ErrMsg>
+                </ErrCard>
+              ) : null}
             </EmailBlock>
             <PasswordBlock>
               <LockIcon fontSize="large" /> &nbsp;
-              <PasswordInput />
+              <PasswordInput
+                id="password"
+                name="password"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+              />
+              {/* エラー表示 */}
+              {formik.touched.password && formik.errors.password ? (
+                <ErrCard>
+                  <ErrMsg>{formik.errors.password}</ErrMsg>
+                </ErrCard>
+              ) : null}
             </PasswordBlock>
-            <LoginButton onClick={loginClick}>ログイン</LoginButton>
+            <LoginButton type="submit">ログイン</LoginButton>
+
+            {/* <LoginButton type="button" onClick={getAccessToken}>
+              // get access_token //{" "}
+            </LoginButton> */}
           </FormContent>
+          {loginURL && <Link href={loginURL}>リンク</Link>}
         </CardContent>
       </GlassCard>
     </LoginLayout>
