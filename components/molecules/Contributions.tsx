3a234001e651304d7db39c64ba2c970474d551e6
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2022-04-18 15:26:51 +0900

    草の並び順を整列

--- components/molecules/Contributions.tsx
+++ components/molecules/Contributions.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
 
@@ -17,106 +18,45 @@ const Contributions = (props: any) => {
   const OutLine = styled.div`
     outline: solid;
     border-radius: 5px;
+    color: blue;
   `;
   console.log(props);
   return (
     <>
       <div>{props.child.totalContributions} contributions in the last year</div>
       <OutLine>
-        {/* {props.child.weeks.map((week: any, index: number) => (
-          <>
-            <div key="index">{week.contributionDays[0].date}</div>
-            <svg width="15" height="15">
-              <rect
-                width="11"
-                height="11"
-                x="0"
-                y="0"
-                fill={`${week.contributionDays[0].color}`}
-              ></rect>
-            </svg>
-          </>
-        ))} */}
-        {props.child.weeks.map((week: any, index: number) => (
-          <>
-            {week.contributionDays.map((contribution: any, index: number) => (
-              <>
-                {/* <div key="index">{contribution.date}</div> */}
-                <svg width="15" height="15">
-                  <rect
-                    width="11"
-                    height="11"
-                    x="0"
-                    y="0"
-                    fill={`${contribution.color}`}
-                  ></rect>
-                </svg>
-              </>
-            ))}
-          </>
-        ))}
-
-        {/* <div key="index">{props.child.weeks[0].contributionDays[1].date}</div>
-          <div key="index">{props.child.weeks[0].contributionDays[2].date}</div>
-          <div key="index">{props.child.weeks[0].contributionDays[3].date}</div>
-          <div key="index">{props.child.weeks[0].contributionDays[4].date}</div>
-          <div key="index">{props.child.weeks[0].contributionDays[5].date}</div>
-          <div key="index">{props.child.weeks[0].contributionDays[6].date}</div>
-          <div key="index">{props.child.weeks[1].contributionDays[0].date}</div>
-          <div key="index">{props.child.weeks[1].contributionDays[1].date}</div>
-          <div key="index">{props.child.weeks[1].contributionDays[2].date}</div>
-          <div key="index">{props.child.weeks[1].contributionDays[3].date}</div>
-          <div key="index">{props.child.weeks[1].contributionDays[4].date}</div>
-          <div key="index">{props.child.weeks[1].contributionDays[5].date}</div>
-          <div key="index">{props.child.weeks[1].contributionDays[6].date}</div> */}
-        {/* <svg width="15" height="15">
-              <rect
-                width="11"
-                height="11"
-                x="0"
-                y="0"
-                fill={`${contribution.contributionDays[0].color}`}
-              ></rect>
-            </svg> */}
-        {/* <div>{contribution.contributionDays[1].date}</div> */}
-        {/* <svg width="15" height="15">
-              <rect
-                width="11"
-                height="11"
-                x="0"
-                y="0"
-                fill={`${contribution.contributionDays[1].color}`}
-              ></rect>
-              <br />
-            </svg> */}
-        {/* <svg width="15" height="15">
-              <rect
-                width="11"
-                height="11"
-                x="0"
-                y="0"
-                fill={`${contribution.contributionDays[2].color}`}
-              ></rect>
-            </svg> */}
-        {/* ))} */}
-        {/* <List> */}
-        {/* <svg>
-        <rect width="11" height="11" x="0" y="0"></rect>
-        <rect width="11" height="11" x="0" y="15"></rect>
-        <rect width="11" height="11" x="0" y="30"></rect>
-        <rect width="11" height="11" x="0" y="45"></rect>
-        <rect width="11" height="11" x="0" y="60"></rect>
-        <rect width="11" height="11" x="0" y="75"></rect>
-        <rect width="11" height="11" x="0" y="90"></rect>
-        <rect width="11" height="11" x="15" y="0"></rect>
-        <rect width="11" height="11" x="15" y="15"></rect>
-        <rect width="11" height="11" x="15" y="30"></rect>
-        <rect width="11" height="11" x="15" y="45"></rect>
-        <rect width="11" height="11" x="15" y="60"></rect>
-        <rect width="11" height="11" x="15" y="75"></rect>
-        <rect width="11" height="11" x="15" y="90"></rect>
-      </svg> */}
-        {/* </List> */}
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "nowrap",
+          }}
+        >
+          {props.child.weeks.map((week: any, index: number) => (
+            <>
+              <div>
+                {week.contributionDays.map(
+                  (contribution: any, index: number) => (
+                    <>
+                      {/* <p key="index">{contribution.date}</p> */}
+                      <div>
+                        <svg width="15" height="15">
+                          <rect
+                            width="11"
+                            height="11"
+                            x="0"
+                            y="0"
+                            fill={`${contribution.color}`}
+                          ></rect>
+                        </svg>
+                      </div>
+                    </>
+                  )
+                )}
+              </div>
+              {/* ここで横並び */}
+            </>
+          ))}
+        </Box>
       </OutLine>
     </>
   );
