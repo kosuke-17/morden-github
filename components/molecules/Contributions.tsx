bc208d935a83ecee76a127c6ce83dd6ccb8c15b7
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2022-04-19 13:52:54 +0900

    contributionエリアにスクロールを追加

--- components/molecules/Contributions.tsx
+++ components/molecules/Contributions.tsx
@@ -10,7 +10,9 @@ import type {
 
 //  styled-components
 // ----------------------------------------------
-
+const ScrollStyle = styled.div`
+  overflow: scroll;
+`;
 // ----------------------------------------------
 type Props = {
   contributions: ContributionType;
@@ -30,66 +32,98 @@ const Contributions: React.FC<Props> = (props) => {
           backgroundColor: "rgba(255, 255, 255, 0.5)",
         }}
       >
-        <CardContent>
-          <Box
-            sx={{
-              display: "flex",
-              flexWrap: "nowrap",
-              justifyContent: "center",
-            }}
-          >
-            {props.contributions.weeks.map((week: any, index: number) => (
-              <>
-                <div key={index}>
-                  {week.contributionDays.map(
-                    (day: ContributionDaysType, index: number) => (
-                      <>
-                        <div key={index}>
-                          <svg width="15" height="15">
-                            <rect
-                              width="11"
-                              height="11"
-                              x="0"
-                              y="0"
-                              fill={`${day.color}`}
-                            ></rect>
-                          </svg>
-                        </div>
-                      </>
-                    )
-                  )}
-                </div>
-              </>
-            ))}
-          </Box>
-          <br />
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-            }}
-          >
-            <div>Less</div>
-            &nbsp;
-            <svg width="15" height="15">
-              <rect width="10" height="10" x="0" y="2" fill={`#ebedf0`}></rect>
-            </svg>
-            <svg width="15" height="15">
-              <rect width="10" height="10" x="0" y="2" fill={`#9be9a8`}></rect>
-            </svg>
-            <svg width="15" height="15">
-              <rect width="10" height="10" x="0" y="2" fill={`#40c463`}></rect>
-            </svg>
-            <svg width="15" height="15">
-              <rect width="10" height="10" x="0" y="2" fill={`#40c463`}></rect>
-            </svg>
-            <svg width="15" height="15">
-              <rect width="10" height="10" x="0" y="2" fill={`#216e39`}></rect>
-            </svg>
-            <div>More</div>
-          </Box>
-        </CardContent>
+        <ScrollStyle>
+          <CardContent>
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "nowrap",
+                justifyContent: "center",
+              }}
+            >
+              {props.contributions.weeks.map((week: any, index: number) => (
+                <>
+                  <div key={index}>
+                    {week.contributionDays.map(
+                      (day: ContributionDaysType, index: number) => (
+                        <>
+                          <div key={index}>
+                            <svg width="15" height="15">
+                              <rect
+                                width="11"
+                                height="11"
+                                x="0"
+                                y="0"
+                                fill={`${day.color}`}
+                              ></rect>
+                            </svg>
+                          </div>
+                        </>
+                      )
+                    )}
+                  </div>
+                </>
+              ))}
+            </Box>
+            <br />
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+              }}
+            >
+              <div>Less</div>
+              &nbsp;
+              <svg width="15" height="15">
+                <rect
+                  width="10"
+                  height="10"
+                  x="0"
+                  y="2"
+                  fill={`#ebedf0`}
+                ></rect>
+              </svg>
+              <svg width="15" height="15">
+                <rect
+                  width="10"
+                  height="10"
+                  x="0"
+                  y="2"
+                  fill={`#9be9a8`}
+                ></rect>
+              </svg>
+              <svg width="15" height="15">
+                <rect
+                  width="10"
+                  height="10"
+                  x="0"
+                  y="2"
+                  fill={`#40c463`}
+                ></rect>
+              </svg>
+              <svg width="15" height="15">
+                <rect
+                  width="10"
+                  height="10"
+                  x="0"
+                  y="2"
+                  fill={`#40c463`}
+                ></rect>
+              </svg>
+              <svg width="15" height="15">
+                <rect
+                  width="10"
+                  height="10"
+                  x="0"
+                  y="2"
+                  fill={`#216e39`}
+                ></rect>
+              </svg>
+              <div>More</div>
+            </Box>
+          </CardContent>
+        </ScrollStyle>
       </Card>
     </>
   );
