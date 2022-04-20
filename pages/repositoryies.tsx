42e2f77c1212a700d71dc9833cd22c8e46a74d6f
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2022-04-20 10:54:58 +0900

    クエリの設定

--- pages/repositoryies.tsx
+++ pages/repositoryies.tsx
@@ -1,6 +1,19 @@
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import React from "react";
+import client from "../apollo-client";
+import { REPOSITORIES_QUERY } from "../common/Query";
 
-const repositoryies = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const  data  = await client.query({ query: REPOSITORIES_QUERY });
+  return {
+    props: data,
+  };
+};
+const repositoryies = ({
+  data,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(data);
+
   return <div>repositoryies</div>;
 };
 
