e8f1c7378d576d9a93df32e08b0b26dde5700b1e
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2022-04-20 14:33:31 +0900

    pinnedのグリッドを調整

--- pages/overview.tsx
+++ pages/overview.tsx
@@ -78,7 +78,7 @@ const overview: React.FC = ({
               columnSpacing={{ xs: 1, sm: 2, md: 3 }}
             >
               {pinnedRipo.map((repo: RepositoryType) => (
-                <Grid item xs={6} key={repo.id}>
+                <Grid item xs={12} md={6} key={repo.id}>
                   <PinnedRepo repo={repo} />
                 </Grid>
               ))}
